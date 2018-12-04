37379b4c89d4ee6eaecd75c57a940344608f6ba7
Irepan Chavez <irepan@alcatrazit.com>
2018-12-04 12:38:34 -0600

    Cambios

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            bye there! Hi again
+            Cambios a esta madre
           </p>
           <a
             className="App-link"
